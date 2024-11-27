5e708b277abc198979f754849db3436aeed1097f
AbdulRauf Siddiqui <abdulrauf_99@outlook.com>
2024-11-27 10:56:48 +0500

    Updated strcture and app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Omer from './pages/Omer';
-import Moiz from './pages/Moiz';
-import Asad from './pages/Asad';
+import Omer from './Pages/Omer';
+import Moiz from './Pages/Moiz';
+import Asad from './Pages/Asad';
+import Login from './Pages/Authentication/Login';
+import ForgotPassword from './Pages/Authentication/ForgotPassword';
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Route path="/moiz" element={<Moiz />} />
         <Route path="/asad" element={<Asad />} />
         <Route path="/omer" element={<Omer />} /> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} /> 
       </Routes>
     </Router>
   );
