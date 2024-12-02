0ef65d45412c54ee248d7a8c3fb132a27eea23b3
AbdulMoiz261 <abdulmoiznouman08@gmail.com>
2024-12-02 12:50:46 +0500

    HyperLink Component

--- frontend/src/Pages/Moiz.js
+++ frontend/src/Pages/Moiz.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import TopNavBar from '../Components/General/TopNavBar';
+import HyperLink from '../Components/General/HyperLink';  // Adjust the path if necessary
 
 function App() {
-  // Define sample data for the parameters
-  const parameters = {
-    background_color: [255, 255, 255],
-    text_color: [0, 0, 0],
-    logo: '/path/to/logo.png',
-    company_name: 'A.R.C.H Labs',
-    company_name_color: [0, 0, 255], // Blue color for the company name
-    username: 'Owner',
-    username_color: [255, 0, 0], // Red color for the username
-  };
-
   return (
     <div>
-      <TopNavBar
-        background_color={parameters.background_color}
-        text_color={parameters.text_color}
-        logo={parameters.logo}
-        company_name={parameters.company_name}
-        company_name_color={parameters.company_name_color}
-        username={parameters.username}
-        username_color={parameters.username_color}
+      <HyperLink
+        text="User Management"
+        path="/all_users"
+        text_color={[56, 90, 87]}
+        width="20%"
+        height="12%"
       />
     </div>
   );
