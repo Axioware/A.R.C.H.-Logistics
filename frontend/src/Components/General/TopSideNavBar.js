5ff6cd649bf97ec1e7c9ffb07f4b0666fa1584bc
AbdulRauf Siddiqui <abdulrauf_99@outlook.com>
2025-01-27 12:12:38 +0500

    testing

--- frontend/src/Components/General/TopSideNavBar.js
+++ frontend/src/Components/General/TopSideNavBar.js
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from "react";
-<<<<<<< HEAD
-// import PropTypes from "prop-types";
-=======
 import PropTypes from "prop-types";
 import {Navigate} from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
->>>>>>> 3218bf829ba4d2acf0bae22578f292aaade5a95b
-import MenuItem from "../General/MenuItem";
+// import MenuItem from "../General/MenuItem";
 import UserDrop from "./UserDrop";
 
 const NavBarWithSidebar = ({
@@ -169,11 +165,7 @@ const NavBarWithSidebar = ({
             }
           .TopSideNavBar-sidebar {
             width: ${isSidebarOpen ? sidebar_width : "0"};
-<<<<<<< HEAD
-            height: ${sidebar_height || "100vh"};
-=======
             height: ${sidebar_height};
->>>>>>> 3218bf829ba4d2acf0bae22578f292aaade5a95b
             background-color: rgb(${sidebar_background_color.join(",")});
             position: fixed;
             height: 100%;
@@ -241,7 +233,7 @@ const NavBarWithSidebar = ({
           }}
           >
 
-          {menuItems.map((menuItem, index) => (
+          {/* {menuItems.map((menuItem, index) => (
             <MenuItem
               key={index}
               icon={menuItem.icon}
@@ -257,7 +249,7 @@ const NavBarWithSidebar = ({
               func={selectedMenuItemChange}
               selectedSubItem={selectedMenuItem}
             />
-          ))}
+          ))} */}
         </div>
         <div className="TopSideNavBar-user-section">
           {isSidebarOpen && <p>{username}</p>}
