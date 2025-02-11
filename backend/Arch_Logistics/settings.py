31c587609b154b6bd45aa30170d6742caedfe212
SyedHassanUlHaq <ulhaqhassan1@gmail.com>
2025-02-11 13:04:31 +0500

    dot env

--- backend/Arch_Logistics/settings.py
+++ backend/Arch_Logistics/settings.py
@@ -16,6 +16,7 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -267,12 +268,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # settings.py
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ulhaqhassan2@gmail.com'  # Replace with your Gmail
-EMAIL_HOST_PASSWORD = 'cdpz tptp bpwc aerq'  # Replace with App Password
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
